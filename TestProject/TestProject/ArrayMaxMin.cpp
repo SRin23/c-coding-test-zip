39cf76695a4b4aa69547a350cf4d4ed2c6c74d35
SRin23 <serin4723@naver.com>
2021-05-15 23:03:04 +0900

    +a

--- TestProject/TestProject/ArrayMaxMin.cpp
+++ TestProject/TestProject/ArrayMaxMin.cpp
@@ -4,9 +4,21 @@ using namespace std;
 int main(){
 	int N = 0;
 	cin >> N;
+	int max = -1;
+	int min = 100;
 	int* arr = (int *)malloc(sizeof(int) * sizeof(N));
 	for (int i = 0; i < N; i++) {
-		
-		arr[i] = 
+		int a = 0;
+		cin >> a;
+		arr[i] = a;
 	}
+	for (int i = 0; i < N; i++) {
+		if (arr[i] >= max) {
+			max = arr[i];
+		}
+		if (arr[i] <= min) {
+			min = arr[i];
+		}
+	}
+	cout << max << ", " << min << endl;
 }
