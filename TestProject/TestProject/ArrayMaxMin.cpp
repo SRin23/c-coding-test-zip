a04f03e565809fca2ab24f79136ccd3ab6e13694
SRin23 <serin4723@naver.com>
2021-05-15 23:00:37 +0900

    5/15commit

---
+++ TestProject/TestProject/ArrayMaxMin.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int N = 0;
+	cin >> N;
+	int* arr = (int *)malloc(sizeof(int) * sizeof(N));
+	for (int i = 0; i < N; i++) {
+		
+		arr[i] = 
+	}
+}
