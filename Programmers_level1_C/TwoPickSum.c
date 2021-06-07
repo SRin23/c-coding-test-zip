556a1c9141cd04f5b115e9e2b2932b5294c6b1df
SRin23 <serin4723@naver.com>
2021-06-07 22:40:16 +0900

    6/7 commit

--- Programmers_level1_C/TwoPickSum.c
+++ Programmers_level1_C/TwoPickSum.c
@@ -15,22 +15,37 @@ void Sort(int* answer, int r) {
 }
 
 int* solution(int numbers[], size_t numbers_len) {
-    int* answer = (int*)malloc(sizeof(int) * 100);
-    memset(answer, 0, sizeof(int) * 100);
+    int* answer;
     int r = 0;
+    int sum = 0;
 
-    for (int i = 0; i < numbers_len; i++) {
+    //메모리 길이 구하기
+    int cnt = 0;
+    int numLength = numbers_len - 1;
+    while (numLength != 0) {
+        cnt += numLength;
+        numLength -= 1;
+    }
+    answer = (int*)malloc(sizeof(int) * cnt);
+
+    //계산
+    for (int i = 0; i < numbers_len - 1; i++) {
         for (int j = i + 1; j < numbers_len; j++) {
-            int sum = 0;
             sum = numbers[i] + numbers[j];
-
-            for (int k = 0; k <= r; k++) {
-                if (answer[k] != sum) {
+            for (int k = 0; k < sizeof(answer); k++) {
+                if (sum == answer[k])
+                    break;
+                else
                     answer[r++] = sum;
-                }
             }
         }
     }
     Sort(answer, r);
     return answer;
+}
+
+int main() {
+    int numbers[] = { 2, 1, 3, 4, 1 };
+    solution(numbers, 5);
+    return 0;
 }
