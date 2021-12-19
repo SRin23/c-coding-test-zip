1bc4d755f9812259042048c413cbc2d88e0a5423
SRin23 <serin4723@naver.com>
2021-12-19 21:37:15 +0900

    문자열을 정수로 바꾸기

--- Programmers_level1_C/StringToInt.c
+++ Programmers_level1_C/StringToInt.c
@@ -1,20 +1,32 @@
+//문자열을 정수로 바꾸기
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* s) {
     int answer = 0;
 
-    for (int i = 0; i < strlen(s); i++) {
-        if (s[0] == '-') {
-            answer = -atoi(s + 1);
-        }
-        else {
-            answer = atoi(s);
-        }
+    //s의 첫번째가 -이면 포인터이므로 s+1부터 맨끝까지 int형으로 변환
+    if (s[0] == '-') {
+        answer = -atoi(s+1);
+    }
+    //s의 첫번째가 +이면 포인터이므로 s+1부터 맨끝까지 int형으로 변환
+    else if (s[0] == '+') {
+        answer = atoi(s+1);
+    }
+    else {
+        answer = atoi(s);
     }
 
-
     return answer;
+}
+int main() {
+    int result = solution("-1234");
+    printf("결과는 %d입니다.\n", result);
+
+    result = solution("12345");
+    printf("결과는 %d입니다.\n", result);
+
+    result = solution("+1295");
+    printf("결과는 %d입니다.\n", result);
 }
